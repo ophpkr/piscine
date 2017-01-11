067ad3c835ac6cf199c96b577f8a75fae44f1216
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2017-01-11 08:08:02 +0100

    Revert "test github"
    This reverts commit 2559b13eab2e7ac53fd48beccb6e98322ef379ee.

--- Vue/head.php
+++ Vue/head.php
@@ -16,7 +16,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="stylesheet" type="text/css" href="assets/css/perso.css">
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-    <!-- test github -->
   crossorigin="anonymous"></script>
   
 
