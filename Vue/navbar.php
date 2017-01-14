c41f89c8b0ebc2d20b778b4b27fb016a892765fa
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2017-01-14 18:06:32 +0100

    Mise en page homepage
    homepage, navbar

--- Vue/navbar.php
+++ Vue/navbar.php
@@ -1,18 +1,17 @@
 
+<nav class="nav" role="navigation">
+  <div class="nav-wrapper container">
+    <a id="logo-container" href="homepage.php" class="brand-logo"><div id="navtitre">Test de Riasec</div></a>
+    <ul class="right hide-on-med-and-down">
+      <li><a class="modal-trigger" href="#modal1"><i class="material-icons right">contacts</i>Connexion</a></li>
+      <li><a class="modal-trigger" href="#modal2"><i class="material-icons right">person_pin</i>Inscription</a></li>
+    </ul>
 
-  <nav class="nav" role="navigation" >
-    <div class="nav-wrapper container">
-    <a id="logo-container" href="homepage.php" class="brand-logo"><img src="images/pp.png" class="responsive-img">Test de RIASEC</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a class="modal-trigger" href="#modal1"><i class="material-icons right">contacts</i>Connexion</a></li>
-        <li><a class="modal-trigger" href="#modal2"><i class="material-icons right">person_pin</i>Inscription</a></li>
-      </ul>
-
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="#modal1"><i class="material-icons right">contacts</i>Connexion</a></li>
-        <li><a href="#modal2"><i class="material-icons right">person_pin</i>Inscription</a></li>
-      </ul>
-      <a href="homepage.php" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
-    </div>
-  </nav>
+    <ul id="nav-mobile" class="side-nav">
+      <li><a href="#modal1"><i class="material-icons right">contacts</i>Connexion</a></li>
+      <li><a href="#modal2"><i class="material-icons right">person_pin</i>Inscription</a></li>
+    </ul>
+    <a href="homepage.php" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+  </div>
+</nav>
 
