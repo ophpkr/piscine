0e47e93e7797a2c207cca4763b0a5e0d62817c9c
Mehdqtx <Mehdi.delvaux@gmail.com>
2017-01-20 14:35:09 +0100

    comm

--- Modele/BD_connexion.php
+++ Modele/BD_connexion.php
@@ -9,8 +9,7 @@ function connexion()
 	$bdd = new PDO('mysql:host=localhost;dbname=piscine;charset=utf8', 'root', '');
 
 	
-	//$bdd = new PDO('mysql:host=127.6.174.130;dbname=nicedriver;charset=utf8', 'adminc57M6b5', 'AWD1MYZZ3tep');
-	//$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
 
 	}
 	catch (Exception $e)
