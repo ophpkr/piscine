58c876cad8a3410f296e6948e7229fa0e36f79d7
Mehdqtx <Mehdi.delvaux@gmail.com>
2017-01-20 14:33:54 +0100

    comm

--- Controlleur/supprimer_resultat.php
+++ Controlleur/supprimer_resultat.php
@@ -3,17 +3,18 @@
 
 require('../Modele/Reponse.php');
 
-       
+       // Si le cookie de l'utilisateur est defini
        if(isset($_COOKIE["user"]))
        {
-
+	// On recupere le numuser dans le cookie
        	$numuser = $_COOKIE["user"];
-
+	
+	// Appel de la fonction de suppresion des réponse d'un utilisateur
        	Supprimer_reponses_user($numuser);
 
        	$msg =" Vos réponses ont été supprimé, vous pouvez refaire le questionnaire";
        	header('Location: ../Vue/code_promo.php?supp= '.$msg);
-		exit();
+		
 
        }
         
